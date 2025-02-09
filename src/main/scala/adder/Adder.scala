33fa002ee5f4630ff91904d7baa8ea0e614efcf3
diadatp <4490283+diadatp@users.noreply.github.com>
2025-02-09 23:21:39 +0000

    docs: add comments to adder module

--- src/main/scala/adder/Adder.scala
+++ src/main/scala/adder/Adder.scala
@@ -14,29 +14,56 @@ import org.chipsalliance.diplomacy.lazymodule.{LazyModule, LazyModuleImp}
 // _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
 import _root_.circt.stage.ChiselStage
 
-// parameters that fully specify the module
+/** Case class defining the parameters for the Adder module.
+  *
+  * @param numAdders
+  *   Number of parallel adders in the module.
+  * @param operandWidth
+  *   Bit width of each operand.
+  */
 case class AdderParams(
-    numAdders: Int, // number of parallel adders
-    operandWidth: Int // bit width of input operands
+    numAdders: Int,
+    operandWidth: Int
 )
 
-// Separate IO Bundle definition from Module as it will be reused often
+/** IO Bundle for the Adder module, defining input and output ports.
+  *
+  * @param params
+  *   Parameters specifying number of adders and operand width.
+  */
 class AdderIO(val params: AdderParams) extends Bundle {
+  // A vector of operand pairs for each parallel adder.
   val operands = Input(
     Vec(params.numAdders, Vec(2, UInt(params.operandWidth.W)))
   )
-  // Wider by one for the carry
+
+  // Sum results, each one bit wider to accommodate carry.
   val sums = Output(Vec(params.numAdders, UInt((params.operandWidth + 1).W)))
 }
 
-// core adder logic as plain Chisel Module
+/** Chisel Module implementing parallel adders.
+  *
+  * @param params
+  *   Specifies the number of adders and operand width.
+  */
 class AdderModule(val params: AdderParams) extends Module {
   val io = IO(new AdderIO(params))
-  io.sums.zip(io.operands) foreach {
+
+  // Perform addition in parallel for all adders with width extension.
+  io.sums.zip(io.operands).foreach {
     case (sum, ops) => { sum := ops.reduce(_ +& _) }
   }
 }
 
+/** TileLink-compatible Adder module with register-mapped interface.
+  *
+  * @param params
+  *   Specifies the number of adders and operand width.
+  * @param address
+  *   Base address for memory-mapped register access.
+  * @param beatBytes
+  *   Number of bytes per beat for TileLink communication.
+  */
 class AdderTileLink(
     params: AdderParams,
     address: AddressSet,
@@ -44,7 +71,8 @@ class AdderTileLink(
 )(implicit p: Parameters)
     extends LazyModule {
 
-  // For linux device tree
+  // Device descriptor for Linux Device Tree compatibility.
+  // Probably irrelevant for bare-metal so the strings are stand-ins.
   val device = new SimpleDevice("adder", Seq("adder"))
 
   val node = TLRegisterNode(
@@ -55,53 +83,63 @@ class AdderTileLink(
 
   override lazy val module = new LazyModuleImp(this) {
 
-    // Create operand registers (numAdders x a/b)
+    // Operand registers to store input values (numAdders x (A, B)).
     val operands = Seq.fill(params.numAdders)(
       (Reg(UInt((beatBytes * 8).W)), Reg(UInt((beatBytes * 8).W))) // (a, b)
     )
 
-    // Instantiate core adder from external repo
+    // Instantiate core adder module.
     val adder = Module(new AdderModule(params))
 
-    // Wire registers to core adder
+    // Connect operand registers to the Adder module inputs.
     operands.zipWithIndex.foreach {
       case ((a, b), idx) => {
         adder.io.operands(idx) := VecInit(a, b)
       }
     }
 
-    // Generate regfields for each adder
+    // Register fields to expose operands and results via TileLink.
     val regFields = operands.zip(adder.io.sums).zipWithIndex.flatMap {
       case (((a, b), sum), idx) =>
         Seq(
+          // Operand A register field
           RegField(
             beatBytes * 8,
-            a, // Operand A
+            a,
             RegFieldDesc(s"a$idx", s"Operand A$idx", reset = Some(0))
           ),
+          // Operand B register field
           RegField(
             beatBytes * 8,
-            b, // Operand B
+            b,
             RegFieldDesc(s"b$idx", s"Operand B$idx", reset = Some(0))
           ),
+          // Sum register field (read-only)
           RegField.r(
             beatBytes * 8,
-            sum, // Sum (read-only)
+            sum,
             RegFieldDesc(s"sum$idx", s"Sum $idx", volatile = true)
           )
         )
     }
 
-    // Map registers to address offsets
+    // Map register fields to memory offsets three at a time.
     node.regmap(regFields.zipWithIndex.map { case (rf, i) =>
       (i * beatBytes, Seq(rf))
     }: _*)
   }
 }
 
+// Configuration key for enabling the Adder module.
 case object AdderEnableKey extends Field[Option[AdderParams]](None)
 
-// defaults are here
+/** Configuration mixin to enable the Adder module in a RocketConfig.
+  *
+  * @param numAdders
+  *   Number of parallel adders.
+  * @param operandWidth
+  *   Bit width of operands.
+  */
 class WithAdder(
     numAdders: Int = 2,
     operandWidth: Int = 32
@@ -114,26 +152,31 @@ class WithAdder(
       )
     })
 
+// Trait to conditionally instantiate an Adder module.
 trait CanHavePeripheryAdder { this: BaseSubsystem =>
 
-  // Check if Adder is enabled via the config
+  // Retrieve Adder configuration parameters (if enabled).
   private val adderParamsOpt = p(AdderEnableKey)
 
+  // Locate the Peripheral Bus (PBUS).
   private val pbus = locateTLBusWrapper(PBUS)
 
+  // Debug print PBUS properties.
   println(
     s"PBUS has beatBytes: ${pbus.beatBytes} and blockBytes: ${pbus.blockBytes}"
   )
 
-  // If enabled, instantiate the Adder and connect it to the PBUS
+  // Instantiate and connect the Adder module if enabled.
   adderParamsOpt.foreach { params =>
     {
       println(
         s"Instantiating Adder with ${params.numAdders} adders of width ${params.operandWidth} bits"
       )
 
+      // Create a synchronous domain for the Adder module.
       val domain = pbus.generateSynchronousDomain.suggestName("adder_domain")
 
+      // Instantiate the TileLink-based Adder module.
       val adder = domain {
         LazyModule(
           new AdderTileLink(
@@ -144,17 +187,18 @@ trait CanHavePeripheryAdder { this: BaseSubsystem =>
         )
       }
 
+      // Connect the Adder module to the PBUS using a TLFragmenter
+      // inside the previously created domain.
       pbus.coupleTo("adderWrite") {
         domain {
           adder.node := TLFragmenter(pbus.beatBytes, pbus.blockBytes)
         } := _
       }
-
     }
   }
 }
 
-// Generate verilog for quick sanity check
+// Object to generate SystemVerilog for testing.
 object AdderModule extends App {
   ChiselStage.emitSystemVerilogFile(
     new AdderModule(new AdderParams(2, 32)),
